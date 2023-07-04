be96d9ace176c911320ecfd301ae47aa8afadb02
André Luferat <contato@luferat.net>
2023-07-03 21:38:08 -0300

    Ajustes

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -15,6 +15,32 @@ export class ContactsComponent {
   contact: any;
   success = false;
 
+  validationMessages: any = {
+    name: {
+      required: 'O nome é obrigatório.',
+      minlength: 'O nome está muito curto.'
+    },
+    email: {
+      required: 'O email é obrigatório.',
+      email: 'Digite um email válido.'
+    },
+    subject: {
+      required: 'O assunto é obrigatório.',
+      minlength: 'O assunto está muito curto.'
+    },
+    message: {
+      required: 'A mensagem é obrigatória.',
+      minlength: 'A mensagem está muito curta.'
+    }
+  }
+
+  formErrors: any = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  }
+
   constructor(
     private formBuilder: FormBuilder,
     private http: HttpClient
@@ -22,15 +48,36 @@ export class ContactsComponent {
 
   ngOnInit(): void {
     this.createForm();
+
+    this.myForm.valueChanges.subscribe(() => {
+      this.updateValidationMessages();
+    });
   }
 
   createForm() {
     this.myForm = this.formBuilder.group({
-      name: ['', Validators.required],
+      name: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
-      subject: ['', Validators.required],
-      message: ['', Validators.required]
+      subject: ['', [Validators.required, Validators.minLength(3)]],
+      message: ['', [Validators.required, Validators.minLength(5)]]
     });
+  }
+
+  updateValidationMessages() {
+    for (const field in this.formErrors) {
+      if (Object.prototype.hasOwnProperty.call(this.formErrors, field)) {
+        this.formErrors[field] = '';
+        const control = this.myForm.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (Object.prototype.hasOwnProperty.call(control.errors, key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
   sendContact() {
@@ -39,17 +86,12 @@ export class ContactsComponent {
     this.contact.date = today.toISOString().replace('T', ' ').split('.')[0];
     this.contact.status = 'received';
 
-    console.log(this.contact)
-
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
       })
     };
 
-    console.log(`${this.env.apiBaseURL}/contacts`, this.contact)
-
-    // Salva na API.
     this.http.post(`${this.env.apiBaseURL}/contacts`, JSON.stringify(this.contact), httpOptions)
       .subscribe(response => {
         this.success = true;
